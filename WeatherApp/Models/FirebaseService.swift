060d36f674352607cd6bd23a36c32dbf68a28bcd
OleksiiKolomiiets <Oleksii_Kolomiiets@epam.com>
2018-08-02 09:43:25 +0300

    added firebase service model.

--- WeatherApp/Models/FirebaseService.swift
+++ WeatherApp/Models/FirebaseService.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+class FirebaseService {
+    
+    static var cities = [CityEntity]()
+    
+    static func getAll(completion: @escaping ([CityEntity], Error?) -> Void) {
+        Firestore.firestore().collection(TableName.city.rawValue).getDocuments { (querySnapshot, error) in
+            var cities = [CityEntity]()
+            var error: Error?
+            querySnapshot?.documents.forEach() { cities.append(CityEntity(with: $0.data())) }
+            self.cities = cities
+            completion(cities, error)
+        }
+    }
+    
+    static func addCity(city: CityEntity, completion: @escaping (Error?) -> Void) {
+        
+        var error: Error?
+        cities.forEach() {
+            if $0.name == city.name { completion(error); return }
+        }
+        Firestore.firestore().collection(TableName.city.rawValue).document().setData(city.dictionary)
+        completion(error)
+    }
+}
